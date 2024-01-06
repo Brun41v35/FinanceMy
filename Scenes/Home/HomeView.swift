8a9fc25eb0a29e893b81339ecfac01cc19b89fd8
Bruno Alves <alves.bruno945@gmail.com>
2024-01-05 23:51:52 -0300

    Add `tableView` in `HomeView`

--- Scenes/Home/HomeView.swift
+++ Scenes/Home/HomeView.swift
@@ -2,6 +2,14 @@ import UIKit
 
 final class HomeView: UIView {
 
+    // MARK: - Private Properties
+
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+
     // MARK: - Init
 
     override init(frame: CGRect) {
@@ -17,11 +25,24 @@ final class HomeView: UIView {
     // MARK: - Private Methods
 
     private func setup() {
+        setupViewHierarchy()
         setupConstraints()
         setupBackgroundColor()
     }
 
-    private func setupConstraints() {}
+    private func setupViewHierarchy() {
+        addSubview(tableView)
+    }
+
+    private func setupConstraints() {
+
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 4),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -4),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8)
+        ])
+    }
 
     private func setupBackgroundColor() {
         backgroundColor = .systemBackground
