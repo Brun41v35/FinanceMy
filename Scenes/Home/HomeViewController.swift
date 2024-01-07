fc3ab2d43d62326ea897af765ea6d4bd93ee7cd6
Bruno Alves <alves.bruno945@gmail.com>
2024-01-07 00:24:06 -0300

    Add `UIBarButtonItem` on `HomeViewController `

--- Scenes/Home/HomeViewController.swift
+++ Scenes/Home/HomeViewController.swift
@@ -10,5 +10,29 @@ final class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setup()
+    }
+
+    // MARK: - Private Methods
+
+    private func setup() {
+        setupNavigationTitle()
+        setupNavigationRightBatButton()
+    }
+
+    private func setupNavigationTitle() {
+        title = "Payment List"
+    }
+
+    private func setupNavigationRightBatButton() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "plus"),
+                                                            style: .plain,
+                                                            target: self,
+                                                            action: #selector(didTapAddPaymentButton))
+    }
+
+    @objc
+    private func didTapAddPaymentButton() {
+        print("Button pressed")
     }
 }
