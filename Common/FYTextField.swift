5d31f56c868544fe183183e28d807e2b87e0162c
Bruno Alves <alves.bruno945@gmail.com>
2024-01-09 22:19:40 -0300

    Add `FYTextField`

---
+++ Common/FYTextField.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+final class FYTextField: UITextField {
+
+    // MARK: - Init
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Private Methods
+
+    private func setup() {
+        textAlignment = .center
+        layer.cornerRadius = 8
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.gray.cgColor
+        textColor = .label
+        tintColor = .label
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        backgroundColor = .systemBackground
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
