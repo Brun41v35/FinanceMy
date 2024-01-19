d28d470a0f5dd97babc7f08353ce036f954a0c4c
Bruno Alves <alves.bruno945@gmail.com>
2024-01-18 22:39:31 -0300

    Binding closure action

--- Scenes/Payment/PaymentViewController.swift
+++ Scenes/Payment/PaymentViewController.swift
@@ -2,14 +2,44 @@ import UIKit
 
 final class PaymentViewController: UIViewController {
 
+    // MARK: - Private Properties
+
+    private let contentView: PaymentViewType
+
+    // MARK: Init
+
+    init(contentView: PaymentViewType = PaymentView()) {
+        self.contentView = contentView
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     // MARK: - LifeCycle
 
     override func loadView() {
-        view = PaymentView()
+        view = contentView
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setup()
+    }
+
+    // MARK: - Private Methods
+
+    private func setup() {
         title = "Pagamento"
+        bindLayoutEvents()
+    }
+
+    private func bindLayoutEvents() {
+
+        contentView.didTapAddPayment = { [weak self] in
+            print("Alou")
+        }
     }
 }
