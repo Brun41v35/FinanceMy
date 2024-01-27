5f5b9e2d33a23ac1c0fda119a7d15da5b18a2151
Bruno Alves <alves.bruno945@gmail.com>
2024-01-26 21:40:59 -0300

    Move `PaymentViewControllerDelegate` protocol to own file

--- Scenes/Payment/PaymentProtocols.swift
+++ Scenes/Payment/PaymentProtocols.swift
@@ -4,3 +4,7 @@ protocol PaymentViewType where Self: UIView {
     var didTapAddPayment: ((PaymentViewModel) -> Void)? { get set }
     var didTapCloseButton: (() -> Void)? { get set }
 }
+
+protocol PaymentViewControllerDelegate: AnyObject {
+    func addNewPayment(cell: PaymentViewModel)
+}
